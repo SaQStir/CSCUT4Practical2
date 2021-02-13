88c1919fcc189873a38c1ec271914daf4089aca7
SaQStir <78617187+SaQStir@users.noreply.github.com>
2021-02-13 19:14:31 +0100

    idk
    - added write / read function
    -added file selection

--- FilesInOut.java
+++ FilesInOut.java
@@ -20,12 +20,47 @@ public class FilesInOut {
 
     public static void main(String[] args)
     {
+        String fileIn = "";
+        String fileOut = "";
 
-        String test = "allison wesley 28011990";
-        System.out.println(updateString(test));
+        boolean fileCheck = false;
 
-        String fileIn = "input.txt";
-        String fileOut = "output.txt";
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("supply filename for input:");
+        do
+            {
+                String input = sc.nextLine();
+                if(checkInputFile(input))
+                {
+                    fileIn = input;
+                    scenario = true;
+                    System.out.println("File successfully located");
+                }
+                else
+                {
+                    System.out.println("Error: please makes sure you input the right file name");
+                }
+        }
+        while (!scenario);
+        scenario = false;
+
+        System.out.println("supply filename for output:");
+        do
+            {
+            String input = sc.nextLine();
+            if(checkOutputFile(input))
+            {
+                fileOut = input;
+                scenario = true;
+                System.out.println("File successfully located");
+            }
+            else
+            {
+                System.out.println("Error: please makes sure you input the right file name");
+            }
+        }
+        while (!scenario);
 
         updateFile(fileIn, fileOut);
 
@@ -34,23 +69,39 @@ public class FilesInOut {
             scenario = true;
         }
 
-        // Replace this with statements to set the file name (input) and file name (output).
-        // Initially it will be easier to hardcode suitable file names.
+    }
+    // main
 
-        // Set up a new Scanner to read the input file.
-        // Processing line by line would be sensible here.
-        // Initially, echo the text to System.out to check you are reading correctly.
-        // Then add code to modify the text to the output format.
+    public static Boolean checkInputFile(String inputFileName)
+    {
+        try
+        {
+                File inputFile = new File(inputFileName);
+                Scanner inFile = new Scanner (inputFile);
+        }
+            catch (IOException e)
+            {
+                System.err.println("IOException: " + e.getMessage() + "not found");
+                return false;
+            }
+        return true;
+    }
+    //checkInputFile
 
-        // Set up a new PrintWriter to write the output file.
-        // Add suitable code into the above processing (because you need to do this line by line also.
-        // That is, read a line, write a line, loop.
-
-        // Finally, add code to read the filenames as arguments from the command line.
-
-        System.out.println("You need to add your own code to do anything");
-
-    } // main
+    public static Boolean checkOutputFile(String outputFileName)
+    {
+        try
+        {
+            File outputFile = new File(outputFileName);
+            Scanner outFile = new Scanner (outputFile);
+        }
+        catch (IOException e)
+        {
+            System.err.println("IOException: " + e.getMessage() + "not found");
+            return false;
+        }
+        return true;
+    }
 
     public static void updateFile(String inputLoc, String outputLoc)
     {
@@ -59,7 +110,7 @@ public class FilesInOut {
         {
             while(sc.hasNextLine())
             {
-                updateFiledWrite(outputLoc,sc.next());
+                updateFiledWrite(outputLoc,sc.nextLine());
             }
         }
         catch(IOException e)
@@ -72,8 +123,9 @@ public class FilesInOut {
         File outputFile = new File(outputloc);
         try (FileWriter fw = new FileWriter(outputFile, true))
         {
-            fw.write(updateString(input) + "\r\n");
-            fw.close();
+            PrintWriter pw = new PrintWriter(fw);
+            pw.write(updateString(input) + "\r\n");
+            pw.close();
         }
         catch(IOException e)
         {
@@ -99,12 +151,17 @@ public class FilesInOut {
             {
                 if( i == 0 || Character.isWhitespace(change.charAt((i-1))))
                 {
+                    if(Character.isWhitespace(change.charAt((i+1))))
+                    {
+                        update = input.charAt(i) + ".";
+                        change.replace(i, i+1, update);
+                    }
                     char temp = Character.toUpperCase(change.charAt(i));
                     change.replace(i, i+1, Character.toString(temp));
                 }
             }
         }
-        if (!scenario)
+        if (scenario)
         {
             return change.toString().toUpperCase();
         }
